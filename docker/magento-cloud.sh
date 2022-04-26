94a5e40987012f4087024be77f5e94445be6d955
Serhii BOREMCHUK <serhii.boremchuk@smile-ukraine.com>
2022-04-26 11:30:55 +0300

    v1.0.7

--- docker/magento-cloud.sh
+++ docker/magento-cloud.sh
@@ -5,3 +5,4 @@ curl -sS https://accounts.magento.cloud/cli/installer | php
 # BEGIN SNIPPET: Magento Cloud CLI configuration
 echo 'export PATH="$HOME/".magento-cloud/bin:"$PATH"' >>/root/.bashrc
 echo 'if [ -f "$HOME/.magento-cloud/shell-config.rc" ]; then . "$HOME/.magento-cloud/shell-config.rc"; fi' >>/root/.bashrc
+source /root/.bashrc
